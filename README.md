0f876bd3512e44871b6e15d46cc557964383f1b3
Siriusmart <71584876+Siriusmart@users.noreply.github.com>
2022-07-21 19:26:41 +0100

    Fixed gif not working in readme

--- README.md
+++ README.md
@@ -1,7 +1,9 @@
-# TUI Additions
+# tui-additions
+
+## TUI Additions
 
 Additions to the TUI crate
 
-![](https://raw.githubusercontent.com/sirusmart/tui-additions/master/assets/framework.gif)
+![](https://raw.githubusercontent.com/Siriusmart/tui-additions/master/assets/framework.gif)
 
 License: GPL-3.0
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 //!
 //! Additions to the TUI crate
 //!
-//! ![](https://raw.githubusercontent.com/sirusmart/tui-additions/master/assets/framework.gif)
+//! ![](https://raw.githubusercontent.com/Siriusmart/tui-additions/master/assets/framework.gif)
 
 /// Additional widgets (structs that impl `tui::widget::Widget`)
 #[cfg(feature = "widgets")]
